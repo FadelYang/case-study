f53b3ad9485089d71fabc80247dd2099870c69fa
fadelanumah <fadelanumah@gmail.com>
2023-10-06 14:32:31 +0700

    feat: Add Triangle Model

---
+++ Models/Triangle.cs
@@ -0,0 +1,10 @@
+namespace Models
+{
+    public class Triangle{
+        public float FirstSide { get; set; }
+
+        public float SecondSide { get; set; }
+
+        public float ThirdSide { get; set; }
+    }
+}
