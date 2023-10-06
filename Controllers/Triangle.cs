0a3152d6b8d8a053a46caecd1b1fe28d7eb90e16
fadelanumah <fadelanumah@gmail.com>
2023-10-06 15:35:21 +0700

    feat: Add Function For Type Of Triangle

--- Controllers/Triangle.cs
+++ Controllers/Triangle.cs
@@ -6,13 +6,33 @@ using Models;
 namespace case_study.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/v1/[controller]")]
     public class TriangleController : ControllerBase
     {
-        [HttpGet]
+        [HttpGet("helloTriangle")]
         public string GetHelloTriangle()
         {
             return $"Hello Triangle";
+        }
+
+        // Equilateral Triangle, has 3 equals side
+        // Isoceles Triangle, has 2 equals side
+        // Scalene Triangle, has no equals side
+        [HttpGet("typeOfTriangle")]
+        public ActionResult<string> TypeOfTriangle(double firstSide, double secondSide, double thirdSide)
+        {
+            if ((firstSide == secondSide) && (firstSide == thirdSide))
+            {
+                return $"It's equilateral triangle";
+            }
+            else if ((firstSide == secondSide) || (firstSide == thirdSide) || (secondSide == thirdSide))
+            {
+                return $"It's isoceles triangle";
+            }
+            else
+            {
+                return $"It's scalene triangle";
+            }
         }
     }
 }
